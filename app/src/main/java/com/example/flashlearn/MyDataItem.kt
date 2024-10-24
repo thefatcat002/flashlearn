a93f98ae43f8c7365b7c5c3d2244f310e831c028
thefatcat002 <josephemilcandelaria59@gmail.com>
2024-10-24 12:10:03 +0800

    new ones again

--- app/src/main/java/com/example/flashlearn/MyDataItem.kt
+++ app/src/main/java/com/example/flashlearn/MyDataItem.kt
@@ -9,8 +9,5 @@ data class MyDataItem(
     val id: Int, // ID of the deck
     val deck: String, // Name of the deck
     val token: String, // Token associated with the deck
-    val token_expires_at: String, // Expiration time for the token
-    val created_at: String, // Creation time of the deck
-    val updated_at: String // Last update time of the deck
 )
 
